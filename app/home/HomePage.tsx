a432fb654293b602efaca6adea39481607f99bb3
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-02-12 09:00:06 -0700

    NEXT: ui lib

--- app/home/HomePage.tsx
+++ app/home/HomePage.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 
 import { BaseProps } from "../../src/utils/build/renderReact";
-import Input from "../components/forms/Input";
-import CsrfInput from "../components/CsrfInput";
-import { User } from "../../src/db/schema";
 type HomeProps = {};
 
 const HomePage: React.FC<BaseProps & HomeProps> = (props) => {
   console.log("[react]: home page props:", props);
   return (
     <div className="min-h-screen bg-l-bg dark:bg-d-bg">
-      <h1>Welcome Home {props.user?.firstName}</h1>
+      <h1>Welcome Back {props.user?.firstName}</h1>
     </div>
   );
 };
